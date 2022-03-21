f496723d0db5f252a139bc6e1dac2ee6dd49689f
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-21 22:26:35 +0530

    [Anuj] Add: Added Indian States Census Code

---
+++ source/repos/AssignmentDay29/AssignmentDay29/CensusAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+        internal Dictionary<string, CensusDTO> LoadCensusData(object iNDIA, string indianStateCensusFilePath, string indianStateCensusHeaders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/repos/AssignmentDay29/AssignmentDay29/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser");
+        }
+    }
+}
