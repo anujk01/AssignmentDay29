110e4ee8b95cf238dccafc6a8f283bd906eb5078
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-21 22:06:44 +0530

    [Anuj] Add: Added States Census Info

---
+++ source/repos/AssignmentDay29/AssignmentDay29/CSVAdapterFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
